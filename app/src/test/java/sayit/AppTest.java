0e9945ec6dbb8b322c5a93e74d20de2067584d97
Simon <simionw42@gmail.com>
2023-05-08 00:10:06 -0700

    Adding all gradle components and new files

---
+++ app/src/test/java/sayit/AppTest.java
@@ -0,0 +1,10 @@
+package sayit;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test void appHasAGreeting() {
+        App classUnderTest = new App();
+    }
+}
