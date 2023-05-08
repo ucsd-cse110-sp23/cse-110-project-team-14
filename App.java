b66f95eaa59c18753a96bf2f42e5af92483f53c0
Simon Wang <66813795+Corus86@users.noreply.github.com>
2023-05-07 19:12:38 -0700

    Added comments

---
+++ App.java
@@ -0,0 +1,65 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+class AppFrame extends JFrame {
+    private JButton askButton; // Create Ask Button
+    private JButton stopButton; // Create Stop Button
+    private AudioRecorder recorder; // Create Listening Device
+
+    AppFrame() {
+        setSize(300, 100); // Set Appframe parameters
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setLayout(new GridLayout(1, 3));
+
+        recorder = new AudioRecorder(); // Initialize buttons and recorder
+
+        askButton = new JButton("Ask Question");
+        this.add(askButton);
+
+        stopButton = new JButton("Stop Question");
+        this.add(stopButton);
+
+        addListeners(); // Give click functionality
+        revalidate(); // Reupdate the frame
+    }
+
+    public void addListeners() {
+    askButton.addActionListener(new ActionListener() { // start recording on click
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            recorder.startRecording();
+        }
+        }
+    );
+    stopButton.addActionListener(new ActionListener() { // stop recording and save the answer as string
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            recorder.stopRecording();
+            Thread t = new Thread( // use another thread for answer computation to not lag UI
+                () -> {
+                try {
+                    String question = Whisper.audioToString();
+                    String answer = ChatGPT.askQuestion(question);
+                    System.out.println(answer);
+                } catch (Exception ex) {
+                    System.out.println("Error occured");
+                }
+            }
+            );
+            t.start(); // start the new thread
+        }
+        }
+    );
+    }
+}
+
+public class App { // main class
+    public static void main(String[] args) throws Exception {
+        new AppFrame(); // call AppFrame
+    }
+}
