fafbc8ac27861a9a80c25eaf5732b1efb3794e69
Simon <simionw42@gmail.com>
2023-05-11 20:26:30 -0700

    Removed extra imports

--- app/src/main/java/sayit/Frame.java
+++ app/src/main/java/sayit/Frame.java
@@ -1,8 +1,6 @@
 package sayit;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
--- app/src/test/java/sayit/ConnectTest.java
+++ app/src/test/java/sayit/ConnectTest.java
@@ -3,8 +3,6 @@ package sayit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import org.junit.jupiter.api.Test;
-
 class ConnectTest {
     void audioFound() {
         assertNotEquals(null, WhisperToChat.connect("ST1.wav"));
