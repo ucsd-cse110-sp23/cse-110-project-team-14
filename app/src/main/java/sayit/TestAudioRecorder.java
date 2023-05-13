3c10800d06514625b57eb61f8bc181609b95a981
Simon <simionw42@gmail.com>
2023-05-12 21:06:22 -0700

    cleanup from main added clear button toggle

--- app/src/main/java/sayit/TestAudioRecorder.java
+++ app/src/main/java/sayit/TestAudioRecorder.java
@@ -1,8 +1,14 @@
 package sayit;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class TestAudioRecorder extends JFrame {
 
