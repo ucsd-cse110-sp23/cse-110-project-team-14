a0e451a8f2e5e25df2afb34d76bfbefb1f30b3be
Danny Nguyen <dan019@ucsd.edu>
2023-05-13 22:48:17 -0700

    Add files via upload

--- app/src/test/java/sayit/Story3Test2.java
+++ app/src/test/java/sayit/Story3Test2.java
@@ -1,13 +1,13 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class Story3Test2 {
-    private Story3Test2 storage;
+    private Storage storage;
 
     @BeforeEach
     public void setup() {
@@ -21,26 +21,17 @@ public class Story3Test2 {
         storage.addQuestion("Question 2", "Answer 2");
 
         // Select a question to delete
-        int selectedQuestionIndex = 0; // Select the first question
-
-        // Get the selected question and answer
-        String selectedQuestion = storage.questions.get(selectedQuestionIndex);
-        String selectedAnswer = storage.answers.get(selectedQuestionIndex);
+        String selectedQuestion = "Question 1";
 
         // Delete the selected question
-        storage.deleteQuestion(selectedQuestionIndex);
+        storage.deleteQuestion(selectedQuestion);
 
         // Verify that the selected question and answer are deleted
-        assertFalse(storage.questions.contains(selectedQuestion));
-        assertFalse(storage.answers.contains(selectedAnswer));
+        assertFalse(storage.questionAnswerMap.containsKey(selectedQuestion));
 
         // Verify that the other question and answer remain in the storage
-        ArrayList<String> expectedQuestions = new ArrayList<>();
-        expectedQuestions.add("Question 2");
-        assertEquals(expectedQuestions, storage.questions);
-
-        ArrayList<String> expectedAnswers = new ArrayList<>();
-        expectedAnswers.add("Answer 2");
-        assertEquals(expectedAnswers, storage.answers);
+        HashMap<String, String> expectedQuestionAnswerMap = new HashMap<>();
+        expectedQuestionAnswerMap.put("Question 2", "Answer 2");
+        assertEquals(expectedQuestionAnswerMap, storage.questionAnswerMap);
     }
 }
